c0d4a6747660880e2fc00e617f0cbc6df961103f
Thais Possamai <100890747+thaispossamaisoares@users.noreply.github.com>
2022-03-29 22:08:34 -0300

    Add files via upload

---
+++ aula29032022/src/aluno.java
@@ -0,0 +1,8 @@
+// ta junto com o arquivo alunoprograma (vetor)
+public class aluno {
+	public String matricula;
+	public String nome;
+	public double nota1;
+	public double nota2;
+	public double nota3;
+}
