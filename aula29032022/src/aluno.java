7386ceebc61428aaa7e168c9e17bea5a360ff8e9
Thais Possamai <100890747+thaispossamaisoares@users.noreply.github.com>
2022-04-12 20:34:15 -0300

    Delete aula29032022 directory

--- aula29032022/src/aluno.java
+++
@@ -1,8 +0,0 @@
-// ta junto com o arquivo alunoprograma (vetor)
-public class aluno {
-	public String matricula;
-	public String nome;
-	public double nota1;
-	public double nota2;
-	public double nota3;
-}
